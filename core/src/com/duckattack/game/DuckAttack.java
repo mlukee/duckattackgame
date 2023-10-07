fe874ac8a4491ba73ff97d46a51578d49c0505c3
Matic Lukežič <maticlukezic007@gmail.com>
2023-10-07 13:01:00 +0200

    Change picture

--- core/src/com/duckattack/game/DuckAttack.java
+++ core/src/com/duckattack/game/DuckAttack.java
@@ -12,12 +12,12 @@ public class DuckAttack extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("worm72.png");
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
